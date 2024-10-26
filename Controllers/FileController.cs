59fbf8f34c6a1ec1420aeb9b9e031040405b647b
Sofia K <119862637+SofiaK031@users.noreply.github.com>
2024-10-26 20:01:27 +0300

    Add files via upload

---
+++ Controllers/FileController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text;
+
+namespace LR7.Controllers
+{
+    public class FileController : Controller
+    {
+        [HttpGet]
+        public IActionResult DownloadFile(string firstName = "", string lastName = "", string fileName = "")
+        {
+            // Перевірка на заповненість полів, тільки якщо дані отримані через GET
+            if (!string.IsNullOrEmpty(firstName) && !string.IsNullOrEmpty(lastName) && !string.IsNullOrEmpty(fileName))
+            {
+                StringBuilder sb = new StringBuilder();
+                sb.Append($"First name: {firstName}\n");
+                sb.Append($"Last name: {lastName}");
+                byte[] bytes = Encoding.UTF8.GetBytes(sb.ToString());
+
+                return File(bytes, contentType: "text/plain", $"{fileName}.txt");
+            }
+            else
+            {
+                // Передача введених значень назад у ViewData
+                ViewData["FirstName"] = firstName;
+                ViewData["LastName"] = lastName;
+                ViewData["FileName"] = fileName;
+
+                // Форма була надіслана, але є пусті поля -> попередження
+                if (Request.Method == "GET" && (!string.IsNullOrEmpty(firstName) || !string.IsNullOrEmpty(lastName) || !string.IsNullOrEmpty(fileName)))
+                {
+                    ViewData["WarningMessage"] = "There are empty fields!";
+                }
+                return View();
+            }
+        }
+    }
+}
